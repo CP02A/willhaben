8bd68b57567838a434c63f9da340f05846cff418
CP02A <ItIsCP02A@gmail.com>
2021-04-21 20:45:52 +0200

    added getListings function

---
+++ app.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch')
+
+/**
+ * gets the listings from an URL
+ * @param {string} url the URL
+ * @returns {object} an array with all the listings
+ */
+const getListings = (url) => {
+    return new Promise((res, rej) => {
+        fetch(url)
+            .then(res => res.text())
+            .then(string => {
+                const temp = string.substr(string.indexOf('window.tmsJson = '))
+                const result = temp.substr(17, temp.indexOf(';')-17)
+                res(JSON.parse(decodeURIComponent(JSON.parse(result).tmsData.search_results)))
+            })
+    })
+}
